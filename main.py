b7bce2ea14bb77a684674d1941b120e30be12048
Carol7033 <carogr210404@gmail.com>
2024-11-02 21:53:59 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from machine import Pin
+import time
+
+# Definimos el LED integrado (en el pin 2 en la mayoría de los ESP32)
+led = Pin(2, Pin.OUT)
+
+# Encendemos y apagamos el LED con una pausa de 1 segundo
+while True:
+    led.on()
+    time.sleep(1)
+    led.off()
+    time.sleep(1)
