503a148f644d7903f339a1e0c3a39c35d0dd66ac
Carol7033 <carogr210404@gmail.com>
2024-11-04 19:27:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ import time
 led = machine.Pin(2, machine.Pin.OUT)
 
 def led_blink():
-    for i in range(5):  # Parpadeo del LED 5 veces
+    while True:
+        # Enciende el LED
         led.on()
-        print("Funciona OTA vers 65 EQUIPO DE Caro, Luis y Fany")
-        time.sleep(2)
+        print("OTA equipo Caro, Luis y Fany")  # Mensaje en consola
+        time.sleep(5)  # Mantiene el LED encendido durante 5 segundos
+
+        # Apaga el LED
         led.off()
-        time.sleep(2)
+        time.sleep(2)  # Mantiene el LED apagado durante 2 segundos
 
 # Llama a la función para comenzar el parpadeo del LED
 led_blink()
+
 
 
 
