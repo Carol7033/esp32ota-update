5107e07dc61e13c153e7f8d7ddc95370111449fa
Carol7033 <carogr210404@gmail.com>
2024-11-04 18:31:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def led_blink():
         # Enciende el LED
         led.on()
         print("Funciona OTA vers 30 EQUIPO DE Caro,Luis y Fany")  # Mensaje en consola
-        time.sleep(5)  # Mantiene el LED encendido durante 
+        time.sleep(10)  # Mantiene el LED encendido durante 
 
         # Apaga el LED
         led.off()
-        time.sleep(3)  # Mantiene el LED apagado durante
+        time.sleep(2)  # Mantiene el LED apagado durante
 
 # Llama a la función para comenzar el parpadeo del LED
 led_blink()
